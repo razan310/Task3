32528eb461d91afca345bdb2779f75351e07dd35
razan kurouni <razankr07@gmail.com>
2024-08-29 03:20:19 +0300

    initial commit

---
+++ src/component/navbar/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import styles from "./navbar.module.css";
+import { NavLink } from 'react-router-dom';
+
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <header>
+            <div className={styles.top_bar}>
+                <div className={styles.contact_info}>
+                    <span><i className="fas fa-envelope"></i> info@company.com</span>
+                    <span><i className="fas fa-map-marker-alt"></i> Sunny Isles Beach, FL 33160</span>
+                </div>
+                <div className={styles.social_icons}>
+                    <a href="#"><i className="fab fa-facebook-f"></i></a>
+                    <a href="#"><i className="fab fa-twitter"></i></a>
+                    <a href="#"><i className="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i className="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <nav className={styles.navbar}>
+                <div className={styles.navbar_brand}>
+                    <h1>VILLA</h1>
+                </div>
+                <div className={styles.menu_toggle} onClick={toggleMenu}>
+                    <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+                </div>
+                <div className={`${styles.navbar_menu} ${menuOpen ? styles.active : ''}`}>
+                    <ul className={styles.navbar_links}>
+                        <li><NavLink className={(isActive) =>isActive ? "active" : ""} to="/ ">Home </NavLink></li>
+                        <li><NavLink className={(isActive) =>isActive ? "active" : ""}  to="/Properties">Properties</NavLink></li>
+                        <li><NavLink className={(isActive) =>isActive ? "active" : ""}  to="/BestDeal">Property Details</NavLink></li>
+                        <li><NavLink className={(isActive) =>isActive ? "active" : ""}  to="/ContactUs">Contact Us</NavLink></li>
+                    </ul>
+                    <div className={styles.schedule_button}>
+                        <a href="#" className={styles.btn_schedule}>Schedule a visit</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
